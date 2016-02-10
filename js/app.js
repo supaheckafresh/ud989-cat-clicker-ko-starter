e408d3aeff6f207a8e5cd02cdc31ec04b256aacf
supaheckafresh <neilstrain@gmail.com>
2016-02-10 13:09:47 -0800

    Display nicknames with control flow foreach

--- js/app.js
+++ js/app.js
@@ -3,6 +3,14 @@ var ViewModel = function () {
 
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Ben');
+    this.nickNames = ko.observableArray([
+            {nickName: 'B-dawg'},
+            {nickName:'BamBam'},
+            {nickName:'Loogaloo'},
+            {nickName:'Boozie'},
+            {nickName:'Benniferous'}
+    ]);
+
     this.imgSrc = ko.observable('./img/22252709_010df3379e_z.jpg');
     this.imgAttribution = ko.observable('www.websitelkjasdf.com');
 
