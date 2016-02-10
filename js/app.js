44b53392ec6cd3e5abcde627d8884f37fc9cb7bd
supaheckafresh <neilstrain@gmail.com>
2016-02-10 15:30:36 -0800

    Create initial cats

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
 
+var initialCats = [
+    { name: 'Sally', nickNames: ['Salster', 'Salty', 'Sabrina'], imgSrc: 'img/pants-cat.jpg', imgAttribution: 'defaultwhatever.com'},
+    { name: 'Frank', nickNames: ['Becky', 'Frankenstein'], imgSrc: 'img/paper-cat.jpg', imgAttribution: 'defaultwhatever.com'},
+    { name: 'Brenard', nickNames: ['Broheim', 'Bennifer', 'Blastocyst'], imgSrc: 'img/box-cat.jpg', imgAttribution: 'defaultwhatever.com'},
+    { name: 'Jennifer', nickNames: ['Yogi', 'Jenny', 'Midas'], imgSrc: 'img/luggage-cat.jpg', imgAttribution: 'defaultwhatever.com'},
+    { name: 'Logan', nickNames: ['Luigi', 'Lombard', 'Lolo'], imgSrc: 'img/hat-cat.jpg', imgAttribution: 'defaultwhatever.com'},
+    { name: 'Grover', nickNames: ['Gro', 'Gandalf', 'Goober'], imgSrc: 'img/box-cat.jpg',imgAttribution: 'defaultwhatever.com' },
+    { name: 'Licky', nickNames: ['Likelike', 'Linda', 'Lefty'], imgSrc: 'img/luggage-cat.jpg', imgAttribution: 'defaultwhatever.com'},
+    { name: 'Mexico', nickNames: ['Pasta', 'Pumba', 'Momo'], imgSrc: 'img/hat-cat.jpg', imgAttribution: 'defaultwhatever.com' }
+];
+
+
 var Cat = function (data) {
     this.clickCount = ko.observable(0);
     this.name = ko.observable(data.name);
@@ -27,18 +39,17 @@ var ViewModel = function () {
 
     var self = this;
 
+    this.catList = ko.observableArray();
+
+    initialCats.forEach(function (catObj) {
+        self.catList.push(new Cat(catObj));
+    });
+
     this.incrementCounter = function () {
-        this.clickCount( this.clickCount() + 1);
+        self.currentCat().clickCount( self.currentCat().clickCount() + 1);
     };
 
-    this.currentCat = ko.observable( new Cat(
-        {
-            name: 'Flabby',
-            nickNames: ['flabbster', 'Frank', 'Fanny', 'Flinolium'],
-            imgSrc: 'img/cat2.jpg',
-            imgAttribution: 'imagesitelkjasf.com'
-        }
-    ) );
+    this.currentCat = ko.observable( this.catList()[0]);
 };
 
 ko.applyBindings(new ViewModel());
